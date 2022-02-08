2c3982b8ab3955529552d8a788a8a7f3a11f8a76
harsona <imharsona@gmail.com>
2022-02-08 22:44:14 +0900

    02. HomeController.java

--- spring_board/src/main/java/com/harsona/spring_board/HomeController.java
+++ spring_board/src/main/java/com/harsona/spring_board/HomeController.java
@@ -4,8 +4,12 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +23,14 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	private SqlSession sqlSession;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+/*	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -35,5 +42,47 @@ public class HomeController {
 		
 		return "home";
 	}
+*/	
+	public String index() {
+		
+		return "/index";
+	}
 	
+	@RequestMapping("/list")
+	public String list(Model model) {
+		IDao dao = sqlSession.getMapper(IDao.class);
+		model.addAttribute("list", dao.listDao());
+		
+		return "/list";
+	}
+	
+	@RequestMapping("/writeForm")
+	public String writeForm() {
+		
+		return "/writeForm";
+	}
+	
+	@RequestMapping("/view")
+	public String view(HttpServletRequest request, Model model) {
+		IDao dao = sqlSession.getMapper(IDao.class);
+		model.addAttribute("view", dao.viewDao(request.getParameter("mId")));
+		
+		return "/view";
+	}
+	
+	@RequestMapping("/delete")
+	public String delete(HttpServletRequest request, Model model) {
+		IDao dao = sqlSession.getMapper(IDao.class);
+		dao.deleteDao(request.getParameter("mId"));
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping("/modify")
+	public String modify(HttpServletRequest request, Model model) {
+		IDao dao = sqlSession.getMapper(IDao.class);
+		dao.mofifyDao(request.getParameter("mName"), request.getParameter("mPhone"), request.getParameter("mAddress"), request.getParameter("mDate"), request.getParameter("mId"));
+		
+		return "redirect:list";
+	}
 }
